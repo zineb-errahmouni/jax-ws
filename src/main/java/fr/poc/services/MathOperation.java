ac6c3554df282fe4b91703b93a26e07ab0838890
zineb <zineb.errahmouni@gmail.com>
2019-02-09 23:03:35 +0100

    clean & format

--- src/main/java/fr/poc/services/MathOperation.java
+++ src/main/java/fr/poc/services/MathOperation.java
@@ -4,14 +4,17 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
-public interface MathOperation{
+public interface MathOperation {
 
     @WebMethod
     double mul(final double a, final double b);
+
     @WebMethod
     double add(final double a, final double b);
+
     @WebMethod
     double substract(final double a, final double b);
+
     @WebMethod
     double devide(final double a, final double b);
 }
